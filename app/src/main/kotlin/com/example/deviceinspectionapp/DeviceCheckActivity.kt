50fa71f1ac9ac04f16e9c13d4e5090a514731d5e
Andrey Golikov <z-a-t-o@yandex.ru>
2024-11-04 22:53:54 +0300

    сохраняет оригинал и миниаюту, установ миниатюры как иконка

--- app/src/main/kotlin/com/example/deviceinspectionapp/DeviceCheckActivity.kt
+++ app/src/main/kotlin/com/example/deviceinspectionapp/DeviceCheckActivity.kt
@@ -5,14 +5,22 @@ import android.content.ContentValues
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Matrix
+import android.media.ExifInterface
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.serialization.json.Json
@@ -26,17 +34,17 @@ class DeviceCheckActivity : AppCompatActivity() {
     private lateinit var uuid: String
     private var currentStageCodeName: String? = null
     private var currentPhotoCodeName: String? = null
+    private var originalPhotoUri: Uri? = null
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_device_check)
 
-        // Получение JSON данных и парсинг
         val jsonData = intent.getStringExtra("jsonData")
         val poverkaData = Json.decodeFromString<PoverkaDTO>(jsonData!!)
         uuid = poverkaData.uuid
 
-        // Инициализация RecyclerView
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerViewStages)
         recyclerView.layoutManager = LinearLayoutManager(this)
         stageAdapter = StageManager(poverkaData.stages) { stageCodeName, photoCodeName ->
@@ -46,15 +54,18 @@ class DeviceCheckActivity : AppCompatActivity() {
         }
         recyclerView.adapter = stageAdapter
 
-        // Настройка запуска камеры
         cameraLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            if (result.resultCode == RESULT_OK) {
-                val photoBitmap = result.data?.extras?.get("data") as Bitmap
-                savePhotoToStorage(photoBitmap)
+            if (result.resultCode == RESULT_OK && originalPhotoUri != null) {
+                val photoBitmap = BitmapFactory.decodeStream(contentResolver.openInputStream(originalPhotoUri!!))
+                if (photoBitmap != null) {
+                    val rotatedBitmap = rotateImageIfRequired(photoBitmap, originalPhotoUri!!)
+                    savePhotoToStorage(rotatedBitmap)
+                } else {
+                    Toast.makeText(this, "Ошибка загрузки фото", Toast.LENGTH_SHORT).show()
+                }
             }
         }
 
-        // Настройка запроса разрешений
         permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
             if (isGranted) {
                 openCamera()
@@ -64,10 +75,6 @@ class DeviceCheckActivity : AppCompatActivity() {
         }
     }
 
-    /**
-     * Обработчик нажатия на иконку камеры.
-     * Проверяет разрешения, если их нет, запрашивает у пользователя.
-     */
     private fun onCameraIconClicked() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
             openCamera()
@@ -76,46 +83,106 @@ class DeviceCheckActivity : AppCompatActivity() {
         }
     }
 
-    /**
-     * Запуск камеры для получения фотографии.
-     */
     private fun openCamera() {
-        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        cameraLauncher.launch(cameraIntent)
-    }
-
-    /**
-     * Сохраняет фотографию в локальное хранилище с уникальным именем файла.
-     *
-     * @param photoBitmap Bitmap изображения для сохранения.
-     */
-    private fun savePhotoToStorage(photoBitmap: Bitmap) {
         val fileName = "${uuid}_${currentStageCodeName}_${currentPhotoCodeName}.jpg"
-
-        // Настройка параметров для сохранения изображения в медиа-хранилище
         val contentValues = ContentValues().apply {
             put(MediaStore.Images.Media.DISPLAY_NAME, fileName)
             put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
             put(MediaStore.Images.Media.RELATIVE_PATH, Environment.DIRECTORY_PICTURES + "/DeviceInspectionApp")
         }
 
-        // Получаем доступ к медиахранилищу
-        val uri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
-        if (uri != null) {
-            var outputStream: OutputStream? = null
-            try {
-                outputStream = contentResolver.openOutputStream(uri)
-                // Сохранение Bitmap в JPEG файл
-                outputStream?.let { photoBitmap.compress(Bitmap.CompressFormat.JPEG, 100, it) }
-                Toast.makeText(this, "Фото сохранено: $fileName", Toast.LENGTH_SHORT).show()
-            } catch (e: IOException) {
-                Toast.makeText(this, "Ошибка сохранения фото", Toast.LENGTH_SHORT).show()
-            } finally {
-                outputStream?.close()
-            }
+        originalPhotoUri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+        if (originalPhotoUri != null) {
+            val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, originalPhotoUri)
+            cameraLauncher.launch(cameraIntent)
         } else {
             Toast.makeText(this, "Ошибка доступа к медиахранилищу", Toast.LENGTH_SHORT).show()
         }
     }
 
+    private fun savePhotoToStorage(photoBitmap: Bitmap) {
+        saveOriginalPhoto(photoBitmap)
+        saveThumbnail(photoBitmap)
+    }
+
+    private fun saveOriginalPhoto(photoBitmap: Bitmap) {
+        if (originalPhotoUri != null) {
+            saveBitmapToUri(photoBitmap, originalPhotoUri!!)
+        } else {
+            Toast.makeText(this, "Ошибка: URI для оригинала не найден", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun saveThumbnail(photoBitmap: Bitmap) {
+        val thumbnail = Bitmap.createScaledBitmap(photoBitmap, 150, 150, true)
+        val thumbnailFileName = "ic_${uuid}_${currentStageCodeName}_${currentPhotoCodeName}.jpg"
+        saveBitmapToStorage(thumbnail, thumbnailFileName)
+
+        val ivPhoto = findViewById<ImageView>(R.id.ivPhoto)
+        ivPhoto.isVisible = true
+        ivPhoto.setImageBitmap(thumbnail)
+    }
+
+    private fun saveBitmapToStorage(bitmap: Bitmap, fileName: String) {
+        val contentValues = ContentValues().apply {
+            put(MediaStore.Images.Media.DISPLAY_NAME, fileName)
+            put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+            put(MediaStore.Images.Media.RELATIVE_PATH, Environment.DIRECTORY_PICTURES + "/DeviceInspectionApp")
+        }
+
+        val uri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+        if (uri != null) {
+            saveBitmapToUri(bitmap, uri)
+        } else {
+            Toast.makeText(this, "Ошибка доступа к медиахранилищу", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun saveBitmapToUri(bitmap: Bitmap, uri: Uri) {
+        var outputStream: OutputStream? = null
+        try {
+            outputStream = contentResolver.openOutputStream(uri)
+            outputStream?.let { bitmap.compress(Bitmap.CompressFormat.JPEG, 100, it) }
+            Toast.makeText(this, "Фото сохранено", Toast.LENGTH_SHORT).show()
+        } catch (e: IOException) {
+            Toast.makeText(this, "Ошибка сохранения фото", Toast.LENGTH_SHORT).show()
+        } finally {
+            outputStream?.close()
+        }
+    }
+
+    /**
+     * Проверяет и поворачивает изображение при необходимости.
+     *
+     * @param bitmap Bitmap изображения для проверки и поворота.
+     * @param photoUri Uri фото для получения метаданных.
+     * @return Повернутый Bitmap, если требуется, иначе оригинальный.
+     */
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun rotateImageIfRequired(bitmap: Bitmap, photoUri: Uri): Bitmap {
+        val inputStream = contentResolver.openInputStream(photoUri)
+        val exif = inputStream?.let { ExifInterface(it) }
+        val orientation = exif?.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL)
+
+        return when (orientation) {
+            ExifInterface.ORIENTATION_ROTATE_90 -> rotateBitmap(bitmap, 90f)
+            ExifInterface.ORIENTATION_ROTATE_180 -> rotateBitmap(bitmap, 180f)
+            ExifInterface.ORIENTATION_ROTATE_270 -> rotateBitmap(bitmap, 270f)
+            else -> bitmap
+        }
+    }
+
+    /**
+     * Поворачивает Bitmap на указанный угол.
+     *
+     * @param bitmap Bitmap изображения для поворота.
+     * @param degrees Угол поворота.
+     * @return Повернутый Bitmap.
+     */
+    private fun rotateBitmap(bitmap: Bitmap, degrees: Float): Bitmap {
+        val matrix = Matrix()
+        matrix.postRotate(degrees)
+        return Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
+    }
 }
