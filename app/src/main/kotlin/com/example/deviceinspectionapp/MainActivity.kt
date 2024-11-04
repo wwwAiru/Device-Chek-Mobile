dcc74739f07bb73a4a046910dd6bd3b003aa709a
Andrey Golikov <z-a-t-o@yandex.ru>
2024-11-04 22:04:14 +0300

    изменил данные на json

--- app/src/main/kotlin/com/example/deviceinspectionapp/MainActivity.kt
+++ app/src/main/kotlin/com/example/deviceinspectionapp/MainActivity.kt
@@ -8,30 +8,40 @@ import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Находим кнопку по её ID
+        // Кнопка для начала новой поверки
         val btnStartInspection: Button = findViewById(R.id.btnStartInspection)
 
-        // Устанавливаем слушатель нажатия
+        // Обработчик нажатия кнопки
         btnStartInspection.setOnClickListener {
-            // Создаем тестовые данные в формате JSON
+            // Создаем тестовые данные, используя новый формат JSON с учетом формата имени файлов
             val testData = PoverkaDTO(
-                caption = "Пример поверки",
+                uuid = "3b45f2a2-d2ad-4a0a-bbcf-68b8e25326cf",
                 stages = listOf(
-                    StageDTO("check1", "Этап 1", description = "", photos = listOf()),
-                    StageDTO("check2", "Этап 2", description = "", photos = listOf())
+                    StageDTO(
+                        stageCodeName = "check1",
+                        caption = "пролив №1",
+                        photos = listOf(
+                            PhotoDTO(
+                                photoCodeName = "gidrometr",
+                                caption = "гидрометр",
+                                imageFileName = "3b45f2a2-d2ad-4a0a-bbcf-68b8e25326cf_check1_gidrometr.jpg"
+                            )
+                        )
+                    )
                 )
             )
             val jsonData = Json.encodeToString(testData)
 
-            // Создаем намерение для перехода на DeviceCheckActivity и передаем JSON
+            // Переход на DeviceCheckActivity с передачей JSON данных
             val intent = Intent(this, DeviceCheckActivity::class.java).apply {
                 putExtra("jsonData", jsonData)
             }
-            startActivity(intent) // Переход на новое активити
+            startActivity(intent)
         }
     }
 }
