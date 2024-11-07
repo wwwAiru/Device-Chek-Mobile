d9114ea031ad28a0f8e0629eca6e96b572565001
Andrey Golikov <z-a-t-o@yandex.ru>
2024-11-07 18:49:20 +0300

    либа

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation("androidx.exifinterface:exifinterface:1.3.6")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
