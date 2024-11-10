274c1dbe3a2bfcc10bfa2aa726e5a0cd66cb92c0
Andrey Golikov <z-a-t-o@yandex.ru>
2024-11-10 22:45:55 +0300

    рефактор тестовых данных

--- app/src/main/kotlin/com/example/deviceinspectionapp/utils/TestData.kt
+++ app/src/main/kotlin/com/example/deviceinspectionapp/utils/TestData.kt
@@ -14,37 +14,30 @@ class TestData private constructor() {
                     StageDTO(
                         stageCodeName = "check1",
                         caption = "пролив №1",
-                        photos = listOf(
-                            PhotoDTO(
-                                photoCodeName = "gidrometr",
-                                caption = "гидрометр",
-                                imageFileName = "3b45f2a2-d2ad-4a0a-bbcf-68b8e25326cf_check1_gidrometr_1.jpg"
-                            ),
-                            PhotoDTO(
-                                photoCodeName = "gidrometr",
-                                caption = "гидрометр",
-                                imageFileName = "3b45f2a2-d2ad-4a0a-bbcf-68b8e25326cf_check1_gidrometr_2.jpg"
-                            )
-                        )
+                        photos = generatePhotos("check1", 10) // Генерация 10 фотографий для стадии
                     ),
                     StageDTO(
                         stageCodeName = "check2",
                         caption = "пролив №2",
-                        photos = listOf(
-                            PhotoDTO(
-                                photoCodeName = "gidrometr",
-                                caption = "гидрометр",
-                                imageFileName = "3b45f2a2-d2ad-4a0a-bbcf-68b8e25326cf_check2_gidrometr_1.jpg"
-                            ),
-                            PhotoDTO(
-                                photoCodeName = "gidrometr",
-                                caption = "гидрометр",
-                                imageFileName = "3b45f2a2-d2ad-4a0a-bbcf-68b8e25326cf_check2_gidrometr_2.jpg"
-                            )
-                        )
+                        photos = generatePhotos("check2", 10) // Генерация 10 фотографий для стадии
                     )
                 )
             )
+        }
+
+        // Функция для генерации фотографий на стадию
+        private fun generatePhotos(stageCodeName: String, photoCount: Int): List<PhotoDTO> {
+            val photos = mutableListOf<PhotoDTO>()
+            for (i in 1..photoCount) {
+                photos.add(
+                    PhotoDTO(
+                        photoCodeName = "gidrometr",
+                        caption = "гидрометр",
+                        imageFileName = "3b45f2a2-d2ad-4a0a-bbcf-68b8e25326cf_${stageCodeName}_gidrometr_$i.jpg"
+                    )
+                )
+            }
+            return photos
         }
     }
 }
