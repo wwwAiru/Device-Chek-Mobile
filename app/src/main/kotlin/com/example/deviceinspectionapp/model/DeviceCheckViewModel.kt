85fa6cce7c194005beaf67a14242bcae883a4480
Andrey Golikov <z-a-t-o@yandex.ru>
2024-11-10 20:16:14 +0300

    выпилил LiveData

--- app/src/main/kotlin/com/example/deviceinspectionapp/model/DeviceCheckViewModel.kt
+++ app/src/main/kotlin/com/example/deviceinspectionapp/model/DeviceCheckViewModel.kt
@@ -1,72 +1,66 @@
 import android.net.Uri
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import java.io.File
 
-class DeviceCheckViewModel : ViewModel() {
+class DeviceCheckViewModel: ViewModel() {
 
-    private val _currentPhotoIndex = MutableLiveData<Int>()
-    val currentPhotoIndex: LiveData<Int> get() = _currentPhotoIndex
+    var currentPhotoIndex: Int = 0
+        private set
+    var uuid: String = ""
+        private set
+    var currentStageCodeName: String = ""
+        private set
+    var currentPhotoCodeName: String = ""
+        private set
+    var originalPhotoUri: Uri? = null
+        private set
+    var originalPhotoFile: File? = null
+        private set
+    private var _thumbnailUris: MutableMap<String, Uri> = mutableMapOf()
+    val thumbnailUris: Map<String, Uri> get() = _thumbnailUris
 
-    private val _uuid = MutableLiveData<String>()
-    val uuid: LiveData<String> get() = _uuid
-
-    private val _currentStageCodeName = MutableLiveData<String>()
-    val currentStageCodeName: LiveData<String> get() = _currentStageCodeName
-
-    private val _currentPhotoCodeName = MutableLiveData<String>()
-    val currentPhotoCodeName: LiveData<String> get() = _currentPhotoCodeName
-
-    private val _originalPhotoUri = MutableLiveData<Uri>()
-    val originalPhotoUri: LiveData<Uri> get() = _originalPhotoUri
-
-    private val _originalPhotoFile = MutableLiveData<File>()
-    val originalPhotoFile: LiveData<File> get() = _originalPhotoFile
-
-    private val _thumbnailUris = MutableLiveData<Map<String, Uri>>()
-    val thumbnailUris: LiveData<Map<String, Uri>> get() = _thumbnailUris
-
+    // Функции для установки значений
     fun setPhotoIndex(index: Int) {
-        _currentPhotoIndex.value = index
+        currentPhotoIndex = index
     }
 
     fun setUuid(uuid: String) {
-        _uuid.value = uuid
+        this.uuid = uuid
     }
 
     fun setStage(stageCodeName: String) {
-        _currentStageCodeName.value = stageCodeName
+        currentStageCodeName = stageCodeName
     }
 
     fun setPhoto(photoCodeName: String) {
-        _currentPhotoCodeName.value = photoCodeName
+        currentPhotoCodeName = photoCodeName
     }
 
     fun setOriginalPhotoUri(uri: Uri) {
-        _originalPhotoUri.value = uri
+        originalPhotoUri = uri
     }
 
     fun setOriginalPhotoFile(file: File) {
-        _originalPhotoFile.value = file
+        originalPhotoFile = file
     }
 
+    // Обновление URI фотографии
     fun updatePhotoUri(stageCodeName: String, photoCodeName: String, thumbnailUri: Uri) {
         val key = "$stageCodeName-$photoCodeName"
-        val currentMap = _thumbnailUris.value ?: emptyMap()
-
-        if (currentMap[key] != thumbnailUri) {
-            val updatedMap = currentMap.toMutableMap()
-            updatedMap[key] = thumbnailUri
-            _thumbnailUris.value = updatedMap
+        if (_thumbnailUris[key] != thumbnailUri) {
+            _thumbnailUris[key] = thumbnailUri
         }
     }
 
+    // Удаление URI по ключу
+    fun removeThumbnailUri(stageCodeName: String, photoCodeName: String) {
+        val key = "$stageCodeName-$photoCodeName"
+        _thumbnailUris.remove(key)
+    }
 
-//    fun updateThumbnailUri(stageCodeName: String, photoCodeName: String, uri: Uri) {
-//        val key = "$stageCodeName-$photoCodeName"
-//        val updatedMap = _thumbnailUris.value?.toMutableMap() ?: mutableMapOf()
-//        updatedMap[key] = uri
-//        _thumbnailUris.value = updatedMap
-//    }
+    // Получение URI по ключу
+    fun getThumbnailUri(stageCodeName: String, photoCodeName: String): Uri? {
+        val key = "$stageCodeName-$photoCodeName"
+        return _thumbnailUris[key]
+    }
 }
