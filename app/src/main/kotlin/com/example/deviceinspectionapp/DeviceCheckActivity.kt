d1adcb9b732394252edeae9455c2022fbdc87ef0
Andrey Golikov <z-a-t-o@yandex.ru>
2024-11-16 02:29:22 +0300

    LinearLayoutManager -> GridLayoutManager

--- app/src/main/kotlin/com/example/deviceinspectionapp/DeviceCheckActivity.kt
+++ app/src/main/kotlin/com/example/deviceinspectionapp/DeviceCheckActivity.kt
@@ -9,7 +9,7 @@ import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.serialization.json.Json
 import java.io.File
@@ -27,28 +27,35 @@ class DeviceCheckActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_device_check)
 
+        // Получаем данные из Intent
         val jsonData = intent.getStringExtra("jsonData")
         photoDirectory = File(intent.getStringExtra("photoDirectoryPath") ?: "")
-        Log.d("", "photoDirectoryPath $photoDirectory")
+        Log.d("DeviceCheckActivity", "photoDirectoryPath: $photoDirectory")
 
         poverkaDTO = Json.decodeFromString(jsonData ?: String())
 
+        // Настроим RecyclerView с GridLayoutManager
         val recyclerView: RecyclerView = findViewById(R.id.recyclerViewStages)
-        recyclerView.layoutManager = LinearLayoutManager(this)
+        val gridLayoutManager = GridLayoutManager(this, 2) // 2 колонки для фото
+        recyclerView.layoutManager = gridLayoutManager
+
+        // Создаем и устанавливаем адаптер
         poverkaAdapter = PoverkaAdapter(this, poverkaDTO)
         recyclerView.adapter = poverkaAdapter
 
+        // Инициализируем launcher для камеры
         takePictureLauncher = setupTakePictureLauncher()
     }
-
 
     @RequiresApi(Build.VERSION_CODES.N)
     private fun setupTakePictureLauncher(): ActivityResultLauncher<CameraCall> {
         return registerForActivityResult(CameraCallResultPassingThrough()) { result ->
             Log.d("CameraResult", "Result: ${result}")
             if (result != null) {
+                // Обрабатываем результат съемки фотографии
                 poverkaAdapter.processPhotoTakenEvent(result)
             } else {
+                // Если фото не было сделано
                 Toast.makeText(this, "Фото не было сделано", Toast.LENGTH_SHORT).show()
                 Log.e("CameraError", "Ошибка при съемке фото")
             }
