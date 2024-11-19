bb1a4835d1216531f43651e1fe0db12563c260d9
Andrey Golikov <z-a-t-o@yandex.ru>
2024-11-19 23:31:15 +0300

    рефакторинг

--- app/src/main/kotlin/com/example/deviceinspectionapp/utils/BitmapUtils.kt
+++ app/src/main/kotlin/com/example/deviceinspectionapp/utils/BitmapUtils.kt
@@ -9,41 +9,75 @@ import android.net.Uri
 import android.os.Build
 import androidx.annotation.RequiresApi
 import java.io.File
+import java.io.InputStream
 
 object BitmapUtils {
 
+    /**
+     * Поворачивает изображение на указанный угол.
+     */
     fun rotateBitmap(bitmap: Bitmap, degrees: Float): Bitmap {
         val matrix = Matrix()
         matrix.postRotate(degrees)
         return Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
     }
 
+    /**
+     * Поворачивает изображение в зависимости от EXIF-ориентации.
+     */
     @RequiresApi(Build.VERSION_CODES.N)
-    fun rotateImageIfRequired(bitmap: Bitmap, photoUri: Uri, contentResolver: ContentResolver): Bitmap {
-        val inputStream = contentResolver.openInputStream(photoUri)
-        val exif = inputStream?.let { ExifInterface(it) }
-        val orientation = exif?.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL)
-        inputStream?.close()
+    fun rotateImageIfRequired(
+        bitmap: Bitmap,
+        photoUri: Uri,
+        contentResolver: ContentResolver
+    ): Bitmap {
+        var inputStream: InputStream? = null
+        return try {
+            inputStream = contentResolver.openInputStream(photoUri)
+            val exif = inputStream?.let { ExifInterface(it) }
+            val orientation = exif?.getAttributeInt(
+                ExifInterface.TAG_ORIENTATION,
+                ExifInterface.ORIENTATION_NORMAL
+            )
 
-        return when (orientation) {
-            ExifInterface.ORIENTATION_ROTATE_90 -> rotateBitmap(bitmap, 90f)
-            ExifInterface.ORIENTATION_ROTATE_180 -> rotateBitmap(bitmap, 180f)
-            ExifInterface.ORIENTATION_ROTATE_270 -> rotateBitmap(bitmap, 270f)
-            else -> bitmap
+            when (orientation) {
+                ExifInterface.ORIENTATION_ROTATE_90 -> rotateBitmap(bitmap, 90f)
+                ExifInterface.ORIENTATION_ROTATE_180 -> rotateBitmap(bitmap, 180f)
+                ExifInterface.ORIENTATION_ROTATE_270 -> rotateBitmap(bitmap, 270f)
+                else -> bitmap
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            bitmap // Если не удается прочитать EXIF, возвращаем оригинал
+        } finally {
+            inputStream?.close()
         }
     }
 
-
+    /**
+     * Создает миниатюру из файла изображения.
+     */
     @RequiresApi(Build.VERSION_CODES.N)
-    fun createThumbnailFromFile(photoFile: File, contentResolver: ContentResolver): Bitmap {
+    fun createThumbnailFromFile(
+        photoFile: File,
+        contentResolver: ContentResolver,
+        thumbWidth: Int = 100,
+        thumbHeight: Int = 100
+    ): Bitmap {
+        // Декодируем исходное изображение
         val originalBitmap = BitmapFactory.decodeFile(photoFile.absolutePath)
         val photoUri = Uri.fromFile(photoFile)
-        // Повернуть изображение, если требуется
+
+        // Поворачиваем изображение, если требуется
         val rotatedBitmap = rotateImageIfRequired(originalBitmap, photoUri, contentResolver)
-        return getResizedBitmap(rotatedBitmap, 100, 100)
+
+        // Уменьшаем изображение до указанных размеров
+        return getResizedBitmap(rotatedBitmap, thumbWidth, thumbHeight)
     }
 
-
+    /**
+     * Изменяет размер изображения до указанных ширины и высоты.
+     */
     private fun getResizedBitmap(bitmap: Bitmap, width: Int, height: Int): Bitmap {
         return Bitmap.createScaledBitmap(bitmap, width, height, true)
     }
