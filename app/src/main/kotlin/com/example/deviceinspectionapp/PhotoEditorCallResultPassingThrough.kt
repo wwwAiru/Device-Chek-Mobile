0fc607e7a884ef6904cc3f5dd5dd2fd680590716
Andrey Golikov <z-a-t-o@yandex.ru>
2024-11-25 23:28:00 +0300

    убрал лишние проверки

--- app/src/main/kotlin/com/example/deviceinspectionapp/PhotoEditorCallResultPassingThrough.kt
+++ app/src/main/kotlin/com/example/deviceinspectionapp/PhotoEditorCallResultPassingThrough.kt
@@ -12,18 +12,6 @@ class PhotoEditorCallResultPassingThrough : ActivityResultContract<PhotoEditorCa
     private lateinit var photoEditorCall: PhotoEditorCall
 
     override fun createIntent(context: Context, input: PhotoEditorCall): Intent {
-        if (input.fileUri.toString().isBlank() || input.destinationUri.toString().isBlank()) {
-            throw IllegalArgumentException(
-                "Необходимо передать корректные URI для обрезки изображения"
-            )
-        }
-
-        if (input.stageIdx < 0 || input.photoIdx < 0) {
-            throw IllegalArgumentException(
-                "Индексы stageIdx и photoIdx должны быть положительными"
-            )
-        }
-
         photoEditorCall = input
         // Формируем Intent для вызова UCrop
         return UCrop.of(input.fileUri, input.destinationUri)
@@ -32,23 +20,13 @@ class PhotoEditorCallResultPassingThrough : ActivityResultContract<PhotoEditorCa
                 setFreeStyleCropEnabled(true)
             })
             .getIntent(context)
-            .apply {
-                putExtra("stageIdx", input.stageIdx)
-                putExtra("photoIdx", input.photoIdx)
-            }
     }
 
 
     override fun parseResult(resultCode: Int, intent: Intent?): PhotoEditorCall? {
         return when {
             resultCode == Activity.RESULT_OK && intent != null -> {
-                val outputUri = UCrop.getOutput(intent)
-                if (outputUri != null) {
-                    photoEditorCall.copy(fileUri = outputUri)
-                } else {
-                    Log.e("UCrop", "Обрезка завершилась без результата (outputUri == null)")
-                    null
-                }
+                photoEditorCall
             }
             resultCode == UCrop.RESULT_ERROR && intent != null -> {
                 val cropError = UCrop.getError(intent)
