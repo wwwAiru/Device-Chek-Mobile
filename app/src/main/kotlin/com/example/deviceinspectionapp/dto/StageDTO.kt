55be4afa8bcee5d39d59f8d2b3761cf042aaf93a
Andrey Golikov <z-a-t-o@yandex.ru>
2024-11-26 22:01:09 +0300

    правка

--- app/src/main/kotlin/com/example/deviceinspectionapp/dto/StageDTO.kt
+++ app/src/main/kotlin/com/example/deviceinspectionapp/dto/StageDTO.kt
@@ -3,8 +3,7 @@ import kotlinx.serialization.Serializable
 /**
  * Класс данных для описания этапа поверки
  * @param stageCodeName кодовое название этапа
- * @param caption название этапа
- * @param description описание этапа
+ * @param caption описание этапа
  * @param photos список фото, сделанных на этапе
  */
 @Serializable
