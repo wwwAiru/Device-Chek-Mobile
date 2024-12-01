9c351f0a5ec1b3b06d2e6277e73b09f50c9ff8a4
Andrey Golikov <z-a-t-o@yandex.ru>
2024-12-01 14:59:58 +0300

    лог exif

--- app/src/main/kotlin/com/example/deviceinspectionapp/StageViewHolder.kt
+++ app/src/main/kotlin/com/example/deviceinspectionapp/StageViewHolder.kt
@@ -14,6 +14,7 @@ import android.widget.TextView
 import androidx.core.content.FileProvider
 import androidx.recyclerview.widget.RecyclerView
 import com.example.deviceinspectionapp.preferences.AppPreferences
+import com.example.deviceinspectionapp.utils.ExifUtils
 import com.google.android.flexbox.FlexboxLayout
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import java.io.File
@@ -177,6 +178,10 @@ class StageViewHolder(
     private fun startPhotoEditing(sourceUri: Uri, stageIdx: Int, photoIdx: Int) {
         val destFile = File(context.photoDirectory, sourceUri.lastPathSegment!!)
         val destUri = Uri.fromFile(destFile)
+
+        val exifDataBeforeEdit = ExifUtils.readExifData(destFile)
+        ExifUtils.logExifData("EXIF BEFORE", exifDataBeforeEdit)
+
         context.editPhotoLauncher.launch(
             PhotoEditorCall(
                 fileUri = sourceUri,
