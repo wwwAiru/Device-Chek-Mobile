5f82b661d436020a647e907a1d0b0cce7f8fa6bf
Andrey Golikov <z-a-t-o@yandex.ru>
2024-12-18 23:12:03 +0300

    fix upload worker

--- app/src/main/kotlin/com/example/deviceinspectionapp/workers/UploadWorker.kt
+++ app/src/main/kotlin/com/example/deviceinspectionapp/workers/UploadWorker.kt
@@ -1,15 +1,19 @@
 package com.example.deviceinspectionapp.workers
 
 import android.content.Context
-import androidx.work.Worker
+import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import com.example.deviceinspectionapp.mainService
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
-class UploadWorker(context: Context, params: WorkerParameters) : Worker(context, params) {
+class UploadWorker(context: Context, params: WorkerParameters) : CoroutineWorker(context, params) {
 
-    override fun doWork(): Result {
+    override suspend fun doWork(): Result {
         return try {
-            mainService.uploadAllPhotos()
+            withContext(Dispatchers.IO) {
+                mainService.uploadAllPhotos()
+            }
             Result.success()
         } catch (e: Exception) {
             Result.failure()
